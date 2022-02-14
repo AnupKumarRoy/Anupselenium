e6c9a754b531d3fff2ce82057b3c661e3e8a2973
AnupKumarRoy <99347275+AnupKumarRoy@users.noreply.github.com>
2022-02-14 20:30:48 +0530

    Add files via upload

---
+++ LaunchUr1.java
@@ -0,0 +1,40 @@
+package WeekDay1;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchUr1 {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://leaftaps.com/opentaps/control/main");
+	driver.manage().window().maximize();
+	driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+	driver.findElement(By.id("password")).sendKeys("crmsfa");
+	driver.findElement(By.className("decorativeSubmit")).click();
+    driver.findElement(By.linkText("CRM/SFA")).click();
+	String title = driver.getTitle();
+	System.out.println(title);
+	driver.findElement(By.linkText("Leads")).click();
+	driver.findElement(By.linkText("Create Lead")).click();
+	driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TESTING");
+	driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Test");
+	driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("anup@gmail.com");
+	driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("9876543210");
+	driver.findElement(By.id("createLeadForm_generalCity")).sendKeys("salem");
+	driver.findElement(By.id("createLeadForm_generalAddress1")).sendKeys("Kavery nagar,mettur");
+
+	
+
+
+
+    
+	
+	
+	
+}
+}
